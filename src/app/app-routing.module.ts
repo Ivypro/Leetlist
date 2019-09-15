703f9b293dea1f867ee5e0f8dbf361cb7d16de85
Brian <Brian@ivypro.com>
2019-09-14 19:47:28 -0700

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from "./home/home.component";
+import { PostComponent } from "./post/post.component";
+import { CitiesComponent } from "./cities/cities.component";
+import { BlankComponent } from "./blank/blank.component";
+import { BarComponent } from "./bar/bar.component";
+
+const routes: Routes = [
+  {
+    path: 'post',
+    component: PostComponent
+  },
+  {
+    path: 'cities',
+    component: CitiesComponent
+  },
+  {
+    path: 'main',
+    component: BlankComponent
+  },
+  {
+    path: 'bar',
+    component: BarComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TopbarComponent } from './topbar/topbar.component';
+import { PostComponent } from './post/post.component';
+import { CitiesComponent } from './cities/cities.component';
+import { BlankComponent } from './blank/blank.component';
+import { BarComponent } from './bar/bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TopbarComponent,
+    PostComponent,
+    CitiesComponent,
+    BlankComponent,
+    BarComponent
   ],
   imports: [
     BrowserModule,
